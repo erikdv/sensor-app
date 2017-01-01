bfd8c17c2cec9674f0333c44604755a4285a1eab
Erik de Vries <erik@edia.nl>
2017-01-01 12:26:09 +0100

    Added temperature sensor

---
+++ app/src/main/java/nl/hva/erik/sensor/TemperatureActivity.java
@@ -0,0 +1,44 @@
+package nl.hva.erik.sensor;
+
+import android.app.Activity;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.Bundle;
+import android.util.Log;
+
+
+public class TemperatureActivity extends Activity implements SensorEventListener {
+
+
+    private SensorManager sensorManager;
+    private Sensor sensor;
+
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_temperature);
+        sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        sensor= sensorManager.getDefaultSensor(Sensor.TYPE_AMBIENT_TEMPERATURE);
+
+        Log.d("TEMP", Float.toString(sensor.getPower()) );
+
+    }
+
+    protected void onResume() {
+        super.onResume();
+        sensorManager.registerListener(this, sensor, SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    protected void onPause() {
+        super.onPause();
+        sensorManager.unregisterListener(this);
+    }
+
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+    }
+
+    public void onSensorChanged(SensorEvent event) {
+    }
+
+}
